7e2f3e085e9f5014777f7d85f16fd4cdd1b01eab
he2e2 <dddd6368@naver.com>
2024-08-23 11:47:12 +0900

    fix: Fixed error

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
